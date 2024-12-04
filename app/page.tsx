44fa5f3d94b92628082a0b72a7a531a6cf868392
Prodan Robert-Alexandru <prodanrobert123@gmail.com>
2024-12-05 01:20:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@
 
 import { navItems } from "@/data";
 
-import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
-import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
+const Hero = dynamic(() => import("@/components/Hero"), { ssr: false });
+const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
+const Footer = dynamic(() => import("@/components/Footer"), { ssr: false });
+const Clients = dynamic(() => import("@/components/Clients"), { ssr: false });
+const Approach = dynamic(() => import("@/components/Approach"), { ssr: false });
+const Experience = dynamic(() => import("@/components/Experience"), { ssr: false });
+const RecentProjects = dynamic(() => import("@/components/RecentProjects"), { ssr: false });
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import dynamic from 'next/dynamic';
 const Home = () => {
